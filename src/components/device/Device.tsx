292bae98622786bccb1a2bc294422663e62edc77
Shreyashi <shreyashi04@gmail.com>
2024-07-21 01:30:48 +0200

    Added a page to dispaly device details

---
+++ src/components/device/Device.tsx
@@ -0,0 +1,36 @@
+import type { Device, Vulnerability } from '@/type';
+import VulnerabilityList from '../VulnerabilityList/VulnerabilityList';
+
+
+interface DeviceDetailsProps {
+  device: Device;
+  vulnerabilities: Vulnerability[];
+}
+const DeviceDetails: React.FC<DeviceDetailsProps> = ({
+  device,
+  vulnerabilities,
+}) => {
+  if (!device) {
+    return <div>Device not found</div>;
+  }
+  return (
+    <div>
+      <p>Hostname{device.hostname}</p>
+      <p>IPv4 Address: {device.ipv4}</p>
+      <p>Operating System: {device.operatingSystem}</p>
+      <p>MAC Address: {device.macAddress}</p>
+      <p>IPv6 Address: {device.ipv6}</p>
+      <h3>Open Ports</h3>
+      <ul>
+        {device.openPorts.map((port) => (
+          <li key={port.number}>
+            {port.number} ({port.protocol})
+          </li>
+        ))}
+      </ul>
+      <h3>Vulnerabilities</h3>
+      <VulnerabilityList vulnerabilities={vulnerabilities} />
+    </div>
+  );
+};
+export default DeviceDetails;
---
+++ src/pages/devices/[id]/index.tsx
@@ -0,0 +1,65 @@
+import DeviceDetails from '@/components/device/Device';
+import { Device } from '@/type';
+import { GetServerSideProps } from 'next';
+
+interface Vulnerability {
+  oid: string;
+  name: string;
+  severity: string;
+  assetId : string;
+  qod : string;
+  solutionType: string;
+}
+interface DeviceDetailsPageProps {
+  device: Device;
+  vulnerabilities: Vulnerability[];
+}
+
+const DeviceDetailsPage: React.FC<DeviceDetailsPageProps> = ({
+  device,
+  vulnerabilities,
+}) => {
+  if (!device) {
+    return <div>Device not found</div>;
+  }
+  return (
+    <div>
+      <h2>Device Details</h2>
+      <DeviceDetails device={device} vulnerabilities={vulnerabilities} />
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+ if(!params){
+  return {
+    props: {
+      device: null,
+      vulnerabilities: null
+    }
+  }
+ }
+ 
+  const deviceResponse = await fetch(`http://localhost:8080/devices/${params.id}`);
+ 
+  if (!deviceResponse.ok) {
+    throw new Error(`Failed to fetch device details: ${deviceResponse.status} ${deviceResponse.statusText}`);
+  }
+  const device: Device[] = await deviceResponse.json();
+  console.log('device details', device)
+  const vulnerabilitiesResponse = await fetch(
+    `http://localhost:8080/devices/${params.id}/vulnerabilities`
+  );
+  if (!vulnerabilitiesResponse.ok) {
+    throw new Error(`Failed to fetch vulnerabilities: ${vulnerabilitiesResponse.status} ${vulnerabilitiesResponse.statusText}`);
+  }
+  const vulnerabilities: Vulnerability[] = await vulnerabilitiesResponse.json();
+  console.log('vulnerabilities',vulnerabilities)
+  return {
+    props: {
+      device,
+      vulnerabilities,
+    },
+  };
+} 
+export default DeviceDetailsPage;
