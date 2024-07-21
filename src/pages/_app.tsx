0177a22914076345d0e1c8c1c6da849081be68a8
Shreyashi <shreyashi04@gmail.com>
2024-07-22 00:43:56 +0200

    Added Mantine UI to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+
+import '@mantine/core/styles.css';
+
+import type { AppProps } from 'next/app';
+import { createTheme, MantineProvider } from '@mantine/core';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
