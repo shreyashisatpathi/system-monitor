7674abf24286efcd3258b64394b602527f020b5d
Shreyashi <shreyashi04@gmail.com>
2024-07-22 18:20:51 +0200

    Added jest cofiguration for unit test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import '@/styles/globals.css';
 import '@mantine/core/styles.css';
 
 import type { AppProps } from 'next/app';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
