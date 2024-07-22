b5b5eea78f79524c3a9c1b99e2ad2f9df5378082
Shreyashi <shreyashi04@gmail.com>
2024-07-22 11:48:40 +0200

    Added an Appshell Layout from mantine UI  for the application

---
+++ src/components/Layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import { NavLink } from '@mantine/core';
+import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+
+const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <div>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm'
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Link href="/">Home</Link>
+          </Group>
+        </AppShell.Header>
+        <main>{children}</main>
+      </AppShell>
+    </div>
+  );
+};
+
+export default Layout;
